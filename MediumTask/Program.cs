73f359392cb1b57b9a1c8c0a8df5265fecb60876
egorc <egorc@LAPTOP-QCHRTMPU>
2022-08-07 19:37:04 +0300

    Добавьте файлы проекта.

---
+++ MediumTask/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace MediumTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int action; // переменная, которая будет отвечать за действие пользователя 
+            //Создаем два танка, первый для пользователя, второй для компьютера
+            Tank tank1 = new Tank(2, 6, 4, 1);
+            Tank tank2 = new Tank(2, 6, 4, 4);
+            Console.WriteLine("Правила игры:" + "\n");
+            Console.WriteLine("Игра представляет собой текстовый бой двух танков");
+            Console.WriteLine("Цель игры: свести жизни вражеского танка к нулю");
+            Console.WriteLine("В данной версии игры предоставляется два варианта действий: выстрел и перезарядка" + "\n");
+
+            Console.WriteLine("Начало игры!");
+            //Вызываем метод для отображения кол-во жизней
+            Console.WriteLine($"Начальные характеристики игрока:");
+            tank1.PrintIndicators();
+            Console.WriteLine($"Начальные характеристики компьютера:");
+            tank2.PrintIndicators();
+            Console.WriteLine();
+
+            //Цикл который выполняется, пока у вражеского танка есть жизни, игра добрая, в данной версии проиграть нельзя)
+            while (tank1.Life >= 0)
+            {
+                Console.WriteLine("Выберете дейсвтие:" + "\n" + "1.Выстрел" + "\n" + "2.Починка" + "\n" + "3.Покупка патронов");
+                action = Convert.ToInt32(Console.ReadLine()); //Переменная для хранения выбранного действия
+                //Цикл обработки выбора действия, если пользователем была введена еденица, то вызовется метод выстрела, если двойка, то метод ремонта
+                if (action == 1)
+                {
+                    if (tank1.Cartridge > 0)
+                    {
+                        if (tank2.Life > 0)
+                        {
+                            tank2.Shot();
+                            tank1.Cartridge--;
+                            //Рандом кривой, на одно действие
+                            Console.WriteLine();
+                            Console.WriteLine("Ход компьютера!");
+                            //Задается рандомное число от 1 до 2, для выбора действия
+                            Random rand = new Random();
+                            float actionComp = rand.Next(1,3);
+                            //Если 1 - выстрел, 2 - ремонт, 3 - покупка патронов
+                            if (actionComp == 1)
+                            {
+                                if (tank2.Cartridge > 0)
+                                { tank1.Shot(); }
+                                else { tank2.Buycartridge(); }
+                                
+                            }
+                            else if (actionComp == 2)
+                            {
+                                tank2.Repair();
+                            }
+                            else
+                            {
+                                tank2.Buycartridge();
+                            }
+                            Console.WriteLine();
+                            actionComp = 0;
+                        }
+                        else Console.WriteLine("Вы выиграли!!");
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Не хватает патронов для выстрела!");
+                    }
+                    
+                }
+                else if (action == 2)
+                {
+                    tank1.Repair(); 
+
+                    Console.WriteLine("Ход компьютера!");
+                    Random rand = new Random();
+                    int actionComp = rand.Next(1, 2);
+                    if (actionComp == 1)
+                    {
+                        tank1.Shot();
+                    }
+                    else
+                    {
+                        tank2.Repair();
+                    }
+                   
+                }
+                else if (action == 3)
+                {
+                    tank1.Buycartridge();
+                    Console.WriteLine("Ход компьютера!");
+                    Random rand = new Random();
+                    int actionComp = rand.Next(1, 2);
+                    if (actionComp == 1)
+                    {
+                        tank1.Shot();
+                    }
+                    else
+                    {
+                        tank2.Repair();
+                    }
+                }    
+                else
+                {
+                    Console.WriteLine("Вы не правильно ввели действие (выберете действие цифрой 1 или 2)");
+                }
+                //Конструкция для продолжения игры, взял с интернета
+                //Ломается при выборе не символа
+                //Предлагается выбрать, продолжить игру или закончить, при вводе символа отличного от n игра будет продолжена
+                Console.WriteLine("Продолжить? y-да, n-нет (y/n)");
+                ConsoleKeyInfo c = Console.ReadKey();
+                Console.WriteLine();
+                if (c.KeyChar == 'n') break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MediumTask/Tank.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediumTask
+{
+    /*
+     Классс для создания экземпляров танка. Содержит в себе методы управления действиями
+    */
+    class Tank
+    {
+        //Свойство танка броня
+        private int armor;
+        //Свойство танка жизни
+        private int life;
+        //Свойство танка урон
+        private int damage;
+        //Свойство кол-во патронов
+        private int cartridge;
+        public int Armor
+        {
+            get { return armor; }
+            set { armor = value; }
+        }
+        public int Life
+        {
+            get { return life; }
+            set { life = value; }
+        }
+        public int Damage
+        {
+            get { return damage; }
+            set { damage = value; }
+        }
+        public int Cartridge
+        {
+            get { return cartridge; }
+            set { cartridge = value; }
+        }
+
+        //Конструктор класса
+        public Tank(int armor, int life, int damage, int catridge) { this.Armor = armor; this.Life = life; this.Damage = damage; this.Cartridge = catridge; }
+
+        //Метод для стрельбы
+        //При вызове метода от запаса жизни танка отнимается четыре(значение урона)
+        public void Shot()
+        {
+
+            int life = Life; //Переменная, в которой будет храниться обновленное значение жизней
+
+            if (life > 0)
+            {
+                life = Life - Damage + Armor;
+                Console.WriteLine("Нанесено 4 урона");
+                Console.WriteLine("Осталось жизней: " + life);
+                Life = life;
+            }
+            else Console.WriteLine("Поражение!");
+        }
+
+        //Метод для починки
+        //При вызове метода к счетчику жизни прибавляется единица
+        public void Repair()
+        {
+            int life = Life;
+            Console.WriteLine(Life);
+            if (Life < life)
+            { 
+                life ++;
+                Console.WriteLine("Жизнь восстановленна!" + "\n" + "Текущее кол-во жизней:" + life);
+            }
+            else { Console.WriteLine("Максимальный запас здоровья! Починка не требуется"); }
+            Life = life;
+        }
+        //Метод купить патроны
+        //При вызове метода к запасу снарядов добавляется один патрон
+        public void Buycartridge()
+        {
+            Cartridge++;
+        }
+
+        //Метод для вывода ко-во жизней и патронов
+        public void PrintIndicators()
+        {
+            Console.WriteLine($"Кол-во жизней {life}");
+            Console.WriteLine($"Кол-во патронов {cartridge}");
+
+        }
+    }
+}
