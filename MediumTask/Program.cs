bb39545674288ecc01b1fd411ef183941d052614
Егор Ципровский <egor.ciprov@gmail.com>
2022-09-13 21:06:05 +0300

    Исправленная версия программы

--- MediumTask/Program.cs
+++ MediumTask/Program.cs
@@ -4,117 +4,205 @@ namespace MediumTask
 {
     class Program
     {
+        enum Action
+        {
+            Damage = 1,
+            Repair = 2,
+            Buy = 3
+        }
+
+
         static void Main(string[] args)
         {
-            int action; // переменная, которая будет отвечать за действие пользователя 
-            //Создаем два танка, первый для пользователя, второй для компьютера
-            Tank tank1 = new Tank(2, 6, 4, 1);
-            Tank tank2 = new Tank(2, 6, 4, 4);
+
             Console.WriteLine("Правила игры:" + "\n");
             Console.WriteLine("Игра представляет собой текстовый бой двух танков");
             Console.WriteLine("Цель игры: свести жизни вражеского танка к нулю");
             Console.WriteLine("В данной версии игры предоставляется два варианта действий: выстрел и перезарядка" + "\n");
 
-            Console.WriteLine("Начало игры!");
-            //Вызываем метод для отображения кол-во жизней
-            Console.WriteLine($"Начальные характеристики игрока:");
-            tank1.PrintIndicators();
-            Console.WriteLine($"Начальные характеристики компьютера:");
-            tank2.PrintIndicators();
-            Console.WriteLine();
 
-            //Цикл который выполняется, пока у вражеского танка есть жизни, игра добрая, в данной версии проиграть нельзя)
-            while (tank1.Life >= 0)
+            Random rnd = new Random();
+
+            //Создаем два танка, первый для пользователя, второй для компьютера
+            Console.WriteLine("Введите броню игрока:");
+            int armor = GetCharacteristics();
+            Console.WriteLine("Введите урон игрока:");
+            int damage = GetCharacteristics();
+            Console.WriteLine("Введите жизни игрока:");
+            int life = GetCharacteristics();
+            Console.WriteLine("Введите броню компьютера:");
+            int pc_armor = GetCharacteristics();
+            Console.WriteLine("Введите урон компьютера:");
+            int pc_damage = GetCharacteristics();
+            Console.WriteLine("Введите жизни компьютера:");
+            int pc_life = GetCharacteristics();
+
+
+            PcTank pcTank = new PcTank();
+            UserTank userTank = new UserTank();
+            pcTank.Print2(ref pc_life, ref pc_damage, ref pc_armor);
+            userTank.Print2(ref life, ref damage, ref armor);
+
+            int initialUserLife = life;
+            int initialPcLife = pc_life;
+
+
+            Console.WriteLine("Доступные действия:" + "\n");
+            Console.WriteLine("1 - Выстрел" + "\n" + "2 - Ремонт(Получаем 1 жизнь)" + "\n" + "3 - Покупка снарядов");
+
+            while (pc_life > 0 || life > 0)
             {
-                Console.WriteLine("Выберете дейсвтие:" + "\n" + "1.Выстрел" + "\n" + "2.Починка" + "\n" + "3.Покупка патронов");
-                action = Convert.ToInt32(Console.ReadLine()); //Переменная для хранения выбранного действия
-                //Цикл обработки выбора действия, если пользователем была введена еденица, то вызовется метод выстрела, если двойка, то метод ремонта
-                if (action == 1)
+                for (int i = 0; i < 1; i++)
                 {
-                    if (tank1.Cartridge > 0)
+                    int user_action = GetAction();
+                    ChoiceUserAction((Action)user_action);
+                    Console.WriteLine("#################" + "\n");
+                    if (life <= 0)
                     {
-                        if (tank2.Life > 0)
-                        {
-                            tank2.Shot();
-                            tank1.Cartridge--;
-                            //Рандом кривой, на одно действие
-                            Console.WriteLine();
-                            Console.WriteLine("Ход компьютера!");
-                            //Задается рандомное число от 1 до 2, для выбора действия
-                            Random rand = new Random();
-                            float actionComp = rand.Next(1,3);
-                            //Если 1 - выстрел, 2 - ремонт, 3 - покупка патронов
-                            if (actionComp == 1)
-                            {
-                                if (tank2.Cartridge > 0)
-                                { tank1.Shot(); }
-                                else { tank2.Buycartridge(); }
-                                
-                            }
-                            else if (actionComp == 2)
-                            {
-                                tank2.Repair();
-                            }
-                            else
-                            {
-                                tank2.Buycartridge();
-                            }
-                            Console.WriteLine();
-                            actionComp = 0;
-                        }
-                        else Console.WriteLine("Вы выиграли!!");
+                        Console.WriteLine("Вы проиграли( ");
+                        Environment.Exit(0);
                     }
-                    else 
+                    if (pc_life <= 0)
                     {
-                        Console.WriteLine("Не хватает патронов для выстрела!");
+                        Console.WriteLine("Вы победили!");
+                        Environment.Exit(0);
                     }
-                    
+                    for (int j = 0; j < 1; j++)
+                    {
+                        Console.WriteLine("Ход компьютера!");
+                        int pc_action = rnd.Next(1, 3);
+                        Console.WriteLine(pc_action);
+                        ChoicePCAction((Action)pc_action);
+                        pcTank.Print2(ref pc_life, ref pc_damage, ref pc_armor);
+                        userTank.Print2(ref life, ref damage, ref armor);
+                    }
                 }
-                else if (action == 2)
-                {
-                    tank1.Repair(); 
-
-                    Console.WriteLine("Ход компьютера!");
-                    Random rand = new Random();
-                    int actionComp = rand.Next(1, 2);
-                    if (actionComp == 1)
-                    {
-                        tank1.Shot();
-                    }
-                    else
-                    {
-                        tank2.Repair();
-                    }
-                   
-                }
-                else if (action == 3)
-                {
-                    tank1.Buycartridge();
-                    Console.WriteLine("Ход компьютера!");
-                    Random rand = new Random();
-                    int actionComp = rand.Next(1, 2);
-                    if (actionComp == 1)
-                    {
-                        tank1.Shot();
-                    }
-                    else
-                    {
-                        tank2.Repair();
-                    }
-                }    
-                else
-                {
-                    Console.WriteLine("Вы не правильно ввели действие (выберете действие цифрой 1 или 2)");
-                }
-                //Конструкция для продолжения игры, взял с интернета
-                //Ломается при выборе не символа
-                //Предлагается выбрать, продолжить игру или закончить, при вводе символа отличного от n игра будет продолжена
-                Console.WriteLine("Продолжить? y-да, n-нет (y/n)");
-                ConsoleKeyInfo c = Console.ReadKey();
-                Console.WriteLine();
-                if (c.KeyChar == 'n') break;
             }
-            Console.ReadKey();
+
+            void ChoiceUserAction(Action action)
+            {
+                switch (action)
+                {
+                    case Action.Damage:
+                        userTank.Shot(ref pc_life, ref damage, ref pc_armor, ref userTank.projectile);
+                        break;
+                    case Action.Repair:
+                        if (life < initialUserLife)
+                        {
+                            userTank.Repair(ref life);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Вы достигли максимального запаса здоровья!");
+                        }
+                        break;
+                    case Action.Buy:
+
+                        userTank.BuyProjectile(ref userTank.projectile);
+                        break;
+
+                }
+
+            }
+
+            void ChoicePCAction(Action action)
+            {
+                switch (action)
+                {
+                    case Action.Damage:
+                        Console.WriteLine("Компьютер применяет действие - выстрел");
+                        pcTank.Shot(ref life, ref pc_damage, ref armor, ref pcTank.pc_projectile);
+                        if (pcTank.pc_projectile <= 0)
+                        {
+                            Console.WriteLine("Ход компьютера! Покупка боеприпасов!");
+                            pcTank.BuyProjectile(ref pcTank.pc_projectile);
+                        }
+                        Console.WriteLine();
+                        break;
+                    case Action.Repair:
+                        Console.WriteLine("Компьютер применяет действие - лечение");
+                        if (pc_life < initialPcLife)
+                        {
+                            pcTank.Repair(ref pc_life);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Максимальный запас здоровья! Перевыбор действия!");
+                            int pc_action = rnd.Next(1, 3);
+                            ChoicePCAction((Action)pc_action);
+                        }
+                        Console.WriteLine();
+                        break;
+                    case Action.Buy:
+                        Console.WriteLine("Компьютер применяет действие - покупка боеприпасов");
+                        pcTank.BuyProjectile(ref pcTank.pc_projectile);
+                        break;
+
+                }
+            }
+
+            static int GetCharacteristics()
+            {
+                int number;
+                string input = Console.ReadLine();
+                bool isConverted = int.TryParse(input, out number);
+                if (isConverted)
+                {
+                    if (number < 1)
+                    {
+                        isConverted = false;
+                    }
+
+                }
+                while (!isConverted)
+                {
+                    Console.WriteLine("Нужно ввести число!");
+                    input = Console.ReadLine();
+                    isConverted = int.TryParse(input, out number);
+                    if (!isConverted)
+                    {
+                        if (number < 1 || number > 100)
+                        {
+                            isConverted = false;
+
+                        }
+                    }
+                }
+                return number;
+
+            }
+            static int GetAction()
+            {
+                int number;
+                Console.WriteLine("Введите действие:");
+                string input = Console.ReadLine();
+                bool isConverted = int.TryParse(input, out number);
+                Console.WriteLine(number);
+                if (isConverted)
+                {
+                    if (number < 1 || number > 3)
+                    {
+                        isConverted = false;
+                    }
+
+                }
+
+                while (!isConverted)
+                {
+                    Console.WriteLine("Для выбора действия введите число (1-выстрел, 2-ремонт)");
+                    input = Console.ReadLine();
+                    isConverted = int.TryParse(input, out number);
+                    if (!isConverted)
+                    {
+                        if (number < 1 || number > 3)
+                        {
+                            isConverted = false;
+                        }
+                    }
+                }
+                return number;
+            }
+
         }
     }
 }
--- MediumTask/Tank.cs
+++ MediumTask/Tank.cs
@@ -11,82 +11,87 @@ namespace MediumTask
     */
     class Tank
     {
-        //Свойство танка броня
-        private int armor;
-        //Свойство танка жизни
-        private int life;
-        //Свойство танка урон
-        private int damage;
-        //Свойство кол-во патронов
-        private int cartridge;
-        public int Armor
-        {
-            get { return armor; }
-            set { armor = value; }
-        }
-        public int Life
-        {
-            get { return life; }
-            set { life = value; }
-        }
-        public int Damage
-        {
-            get { return damage; }
-            set { damage = value; }
-        }
-        public int Cartridge
-        {
-            get { return cartridge; }
-            set { cartridge = value; }
-        }
 
-        //Конструктор класса
-        public Tank(int armor, int life, int damage, int catridge) { this.Armor = armor; this.Life = life; this.Damage = damage; this.Cartridge = catridge; }
-
-        //Метод для стрельбы
-        //При вызове метода от запаса жизни танка отнимается четыре(значение урона)
-        public void Shot()
+        public virtual void Print2(ref int life, ref int damage, ref int protection) => Console.WriteLine($"Броня: {protection} Жизни:{life} Урон: {damage}");
+        public virtual int Shot(ref int life, ref int damage, ref int protection, ref int projectile)
         {
-
-            int life = Life; //Переменная, в которой будет храниться обновленное значение жизней
-
-            if (life > 0)
+            Console.WriteLine(projectile);
+            if (projectile <= 0)
             {
-                life = Life - Damage + Armor;
-                Console.WriteLine("Нанесено 4 урона");
-                Console.WriteLine("Осталось жизней: " + life);
-                Life = life;
+                Console.WriteLine("Закончились боеприпасы! Не возможно произвести выстрел!");
             }
-            else Console.WriteLine("Поражение!");
-        }
-
-        //Метод для починки
-        //При вызове метода к счетчику жизни прибавляется единица
-        public void Repair()
-        {
-            int life = Life;
-            Console.WriteLine(Life);
-            if (Life < life)
-            { 
-                life ++;
-                Console.WriteLine("Жизнь восстановленна!" + "\n" + "Текущее кол-во жизней:" + life);
+            else
+            {
+                life = (life + protection - damage);
+                projectile--;
             }
-            else { Console.WriteLine("Максимальный запас здоровья! Починка не требуется"); }
-            Life = life;
-        }
-        //Метод купить патроны
-        //При вызове метода к запасу снарядов добавляется один патрон
-        public void Buycartridge()
-        {
-            Cartridge++;
+            return life;
         }
 
-        //Метод для вывода ко-во жизней и патронов
-        public void PrintIndicators()
+        public void Repair(ref int life)
         {
-            Console.WriteLine($"Кол-во жизней {life}");
-            Console.WriteLine($"Кол-во патронов {cartridge}");
+            life++;
 
+        }
+        public virtual void BuyProjectile(ref int projectile)
+        {
+            projectile++;
         }
     }
+
+    class PcTank : Tank
+    {
+        public int pc_life;
+        public int pc_damage;
+        public int pc_protection;
+        public int pc_projectile;
+
+
+        public PcTank()
+        {
+            pc_damage = 0;
+            pc_life = 0;
+            pc_protection = 0;
+            pc_projectile = 0;
+        }
+        public override void Print2(ref int pc_life, ref int pc_damage, ref int pc_protection) => Console.WriteLine($"Броня противника: {pc_protection} Жизни противника:{pc_life} Урон противника: {pc_damage} Кол-во боеприпасов противника: {pc_projectile}");
+        public override int Shot(ref int life, ref int damage, ref int protection, ref int pc_projectile)
+        {
+            if (pc_projectile <= 0)
+            {
+                Console.WriteLine("Закончились боеприпасы! Не возможно произвести выстрел! Следующим действием купите припасы! ");
+            }
+            else
+            {
+                life = (life + protection - damage);
+                pc_projectile--;
+            }
+            return life;
+        }
+        public override void BuyProjectile(ref int pc_projecint)
+        {
+            pc_projectile++;
+        }
+    }
+
+    class UserTank : Tank
+    {
+        public int life;
+        public int damage;
+        public int protection;
+        public int projectile;
+
+
+        public UserTank()
+        {
+            life = 0;
+            damage = 0;
+            protection = 0;
+            projectile = 0;
+        }
+        public override void Print2(ref int life, ref int damage, ref int protection) => Console.WriteLine($"Ваша Броня: {protection} Ваши Жизни:{life} Ваш Урон: {damage} Ваши боеприпасы: {projectile}");
+
+
+    }
 }
+
